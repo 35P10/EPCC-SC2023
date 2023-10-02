cc3f83d422135c695c387a5fda7d47e79d0ae185
villanueva <villanueva.guerrero@outlook.com>
2023-10-02 13:16:39 -0500

    change deploy base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EPCC-SC2023/'
+  base: '/35P10.github.io/'
 })
